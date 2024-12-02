83bd111effa7282d60038c6882abf1b81a0292cc
rubeshudayakumar <rubesh.udayakumar@cdw.com>
2024-12-02 17:17:47 +0530

    recursion

--- src/StringPattern.java
+++ src/StringPattern.java
@@ -1,2 +1,22 @@
+import java.util.Scanner;
+
 public class StringPattern {
+
+    private final String message;
+    private final int limit;
+
+    private StringPattern(String message,int limit){
+        this.message = message;
+        this.limit = limit;
+    }
+
+    private void printPattern(){
+        System.out.println(message.substring(0,limit)+message.substring(message.length()-limit));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        StringPattern stringPattern = new StringPattern(scanner.next(),scanner.nextInt());
+        stringPattern.printPattern();
+    }
 }
